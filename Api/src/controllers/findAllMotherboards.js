cdab726810c02192213aa6ac6b2585e0a4a5ff30
Jonavoe <voeffray.jonathan@gmail.com>
2023-04-07 15:00:16 -0300

    creacion de bdd

--- Api/src/controllers/findAllMotherboards.js
+++
@@ -1,8 +0,0 @@
-const { Motherboards } = require('../db');
-
-const findAllMotherboards = async (query) => {
-	const motherboards = await Motherboards.findAll();
-	return motherboards;
-};
-
-module.exports = findAllMotherboards;
