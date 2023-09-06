519eedb37e2a8138d91a9f1a3400e3a7957d38c7
Jonavoe <voeffray.jonathan@gmail.com>
2023-09-06 18:15:58 -0300

    Update db.js

--- Api/src/db.js
+++ Api/src/db.js
@@ -1,14 +1,22 @@
-require('dotenv').config();
+require("dotenv").config();
 const { DB_USER, DB_PASSWORD, DB_HOST, DB_PORT, DB_BDD } = process.env;
-const { Sequelize } = require('sequelize');
-const Mouses = require('./models/Mouses');
-const Motherboards = require('./models/Motherboards');
-const Teclado = require('./models/Teclado');
-const Other = require('./models/Other');
+const { Sequelize } = require("sequelize");
+const Mouses = require("./models/Mouses");
+const Motherboards = require("./models/Motherboards");
+const Teclado = require("./models/Teclado");
+const Other = require("./models/Other");
 
 const database = new Sequelize(
-	`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_BDD}`,
-	{ logging: false }
+  `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_BDD}`,
+  {
+    logging: false,
+    dialectOptions: {
+      ssl: {
+        require: true, // Habilitar SSL
+        rejectUnauthorized: false, // Opcional: permite conexiones no autenticadas (puede variar según la configuración de Render)
+      },
+    },
+  }
 );
 
 Mouses(database);
@@ -19,6 +27,6 @@ Other(database);
 // const { Teclado, Motherboards, Mouses } = database.models;
 
 module.exports = {
-	database,
-	...database.models,
+  database,
+  ...database.models,
 };
